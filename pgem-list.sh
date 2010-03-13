3d1a81ecd5ef369c39ae39e0e9f14d78ca7e825a
Ryan Tomayko <rtomayko@gmail.com>
2010-03-13 01:32:47 -0800

    pgem-list takes multiple glob patterns to match

--- pgem-list.sh
+++ pgem-list.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
-#/ Usage: pgem-list
-#/ List installed packages
 set -e
+usage="Usage: pgem-list [<glob>...]
+List installed packages matching <glob>s or all packages with no <glob>s."
+expr "$*" : '.*--help' >/dev/null &&
+echo "$usage" && exit 2
 
 . pgem-sh-setup
 
-for f in $PGEMDB/*/active
+# With no <globs>, list everything.
+[ "$*" ] || set -- '*'
+
+# Run over globs gives on the command line and locate matching
+# installed packages.
+for glob in "$@"
 do
-    name=$(basename $(dirname $f))
-    vers=$(readlink $f)
-    echo "$name $vers"
-done | sort
+    ls -1 "$PGEMDB"/$glob/active 2>/dev/null |
+    while read path
+    do
+        name=$(basename $(dirname $path))
+        vers=$(readlink $path)
+        echo "$name $vers"
+    done
+done |
+
+# Don't show packages multiple times.
+sort -u
