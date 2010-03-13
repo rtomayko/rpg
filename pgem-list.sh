9e45ee66c7b9c0c6c0fdec936987c4202274208d
Ryan Tomayko <rtomayko@gmail.com>
2010-03-13 01:55:12 -0800

    pgem-list -x writes lines for non-matching globs

--- pgem-list.sh
+++ pgem-list.sh
@@ -1,26 +1,45 @@
 #!/bin/sh
 set -e
-usage="Usage: pgem-list [<glob>...]
-List installed packages matching <glob>s or all packages with no <glob>s."
+usage="Usage: pgem-list [-x] [<glob>...]
+List installed packages matching <glob>s or all packages with no <glob>s.
+
+Options
+  -x                    Include non-matching globs in output."
 expr "$*" : '.*--help' >/dev/null &&
 echo "$usage" && exit 2
 
 . pgem-sh-setup
 
+shownonmatch=false
+test "$1" = '-x' && {
+    shownonmatch=true
+    shift
+}
+
 # With no <globs>, list everything.
 [ "$*" ] || set -- '*'
+
+# Switch into package database dir or bail out if it doesn't exist.
+test -d "$PGEMDB" &&
+cd "$PGEMDB" || exit 0
 
 # Run over globs gives on the command line and locate matching
 # installed packages.
 for glob in "$@"
 do
-    ls -1 "$PGEMDB"/$glob/active 2>/dev/null |
-    while read path
+    matched=false
+    for path in $(ls -1 $glob/active 2>/dev/null)
     do
-        name=$(basename $(dirname $path))
+        matched=true
+        package=${path%/active}
         vers=$(readlink $path)
-        echo "$name $vers"
+        printf "%-30s %s\n" $package $vers
     done
+
+    # Write a line of output for the failed glob with `-x`.
+    if $shownonmatch && ! $matched
+    then printf "%-30s %s\n" "$glob" "X"
+    fi
 done |
 
 # Don't show packages multiple times.
