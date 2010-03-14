f5139b1d4c5b5ded57d4a2f4fb9e477e698a5f00
Ryan Tomayko <rtomayko@gmail.com>
2010-03-14 09:37:38 -0700

    rpg-steal passes all packages to install now

--- rpg-steal.sh
+++ rpg-steal.sh
@@ -9,7 +9,7 @@ Transplant packages from Rubygems to rpg.
 With the -n option, show what would be installed but do not.'
 
 if test "$1" = '-n'
-then  tampon="echo"
+then  tampon="-n 2 echo"
       shift
 else  tampon="rpg-install"
 fi
@@ -18,4 +18,4 @@ gem list --local                                             |
 sed "s|^\(${GEMNAME_BRE}\) *(\([$GEMVERS_BRE\).*|GEM \1 \2|" |
 grep '^GEM '                                                 |
 sed 's/^GEM //'                                              |
-xargs -n 2 "$tampon"
+xargs $tampon
