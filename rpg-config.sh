7c0efd1bbcba0c3e029dcfe858477b7fa6877136
Ryan Tomayko <rtomayko@gmail.com>
2010-03-18 16:40:34 -0700

    rpg-config output broken up and commented

--- rpg-config.sh
+++ rpg-config.sh
@@ -6,4 +6,26 @@ ARGV="$@"
 USAGE '${PROGNAME}
 Write rpg configuration values to standard output.'
 
-env | grep ^RPG | sort
+cat <<CONFIG
+# rpg installs package contents in these locations:
+RPGBIN='$RPGBIN'
+RPGLIB='$RPGLIB'
+RPGMAN='$RPGMAN'
+
+# rpg keeps its package database, index, and gem cache in these locations:
+RPGPATH='$RPGPATH'
+RPGDB='$RPGDB'
+RPGINDEX='$RPGINDEX'
+RPGPACKS='$RPGPACKS'
+RPGCACHE='$RPGCACHE'
+
+# rpg sources these configuration files before executing commands:
+RPGSYSCONF='$RPGSYSCONF'
+RPGUSERCONF='$RPGUSERCONF'
+
+# rpg uses these options to control various aspects of its behavior:
+RPGTRACE='$RPGTRACE'
+RPGSHOWBUILD='$RPGSHOWBUILD'
+RPGSTALETIME='$RPGSTALETIME'
+RPGSPECSURL='$RPGSPECSURL'
+CONFIG
