b3c0ffd59f3a5084ba1e6a6520e9fff5d792bf41
Ryan Tomayko <rtomayko@gmail.com>
2010-03-19 00:47:20 -0700

    document dep solve loop and rebuild master package list each time

--- rpg-prepare.sh
+++ rpg-prepare.sh
@@ -37,6 +37,8 @@ rpg-sync -s
 RPGSESSION="$RPGDB"
 sessiondir="$RPGSESSION/@$session"
 packlist="$sessiondir/package-list"
+solved="$sessiondir/solved"
+existing="$sessiondir/pre-existing"
 
 # Get rid of any crusty session directory and then create a new one. It might be
 # cool to add an `-e` option so that sessions could be edited to add or
@@ -60,21 +62,18 @@ done > "$sessiondir/argv"
 # corresponding to a dependency have the depending package name in the first
 # field instead of `@user`.
 notice "writing user package-list"
-rpg-package-list "$@"        |
-sort -u                      |
-sed "s/^/@user /"            > "$packlist"
+rpg-package-list "$@" |
+sort -b -u            |
+sed "s/^/@user /"     > "$packlist"
 
 # Create the pre-existing package index. This is a simple list of all packages
 # currently installed in the standard `<package> <version>` package index
 # format. We try to resolve packages and dependencies against this list before
 # the main release index.
 notice "writing pre-existing package index"
-rpg-package-index > "$sessiondir/pre-existing"
+rpg-package-index > "$existing"
 
-# Dependency Resolution
-# ---------------------
-
-# Tell the user we're about to begin.
+# Grab some stats and let 'em know we're about to begin.
 numpacks=$(sed -n '$=' <"$packlist")
 if test $numpacks -eq 1
 then packname=$(head -1 "$packlist" |cut -d ' ' -f 2)
@@ -82,55 +81,73 @@ then packname=$(head -1 "$packlist" |cut -d ' ' -f 2)
 else heed "calculating dependencies for $numpacks package(s) ..."
 fi
 
-cat </dev/null >"$sessiondir/solved"
+# Dependency Solving
+# ------------------
+#
+# Dependency solving and conflict resolution works in multiple passes
+# over the master package list. On each iteration, concrete package versions
+# are resolved from the version requirements in the package list. Each resolved
+# packages's dependency rules are then added to the master package list. If no
+# rules are added to the master package list in an iteration then the solving is
+# complete.
 
+: >"$solved"
 changed=true
 runcount=0
 while $changed
 do
     runcount=$(( runcount + 1 ))
     notice "this is depsolve run #$runcount"
-    cat </dev/null >"$packlist+" # truncate
 
-    cut -d ' ' -f 2- "$packlist"                             |
-    sort -b -u                                               |
-    rpg-solve -u "$sessiondir/solved" \
-                 "$sessiondir/pre-existing"                  |
-    sort -b >"$sessiondir/solved+"
-    mv "$sessiondir/solved+" "$sessiondir/solved"
+    # Solve all packages in the master package list and write the resulting
+    # package index to the newly solved file (`solved+`). The solved file is
+    # a sorted package index in `<name> <version>` format.
+    cut -d ' ' -f 2- "$packlist"             |
+    uniq                                     |
+    rpg-solve -u "$solved" "$existing"       >"$solved+"
 
-    grep -v -- ' -$' <"$sessiondir/solved"                   |
-    xargs -P 4 -n 2 rpg-fetch >/dev/null
+    # Use `comm(1)` to select only those lines in the newly solved file that
+    # were not present on the previous iteration, exclude packages that could
+    # not be solved to a concrete version, and pass the remaining
+    # package/version combos into `rpg-package-register` to fetch and enter
+    # the package into the database. Using `xargs -P 8` allows as many as eight
+    # concurrent fetch/register operations to run in parallel.
+    comm -13 "$solved" "$solved+"            |
+    grep -v ' -$'                            |
+    xargs -P 8 -n 2 rpg-package-register     >/dev/null
 
-    grep -v -- ' -$' <"$sessiondir/solved"                   |
-    while read package version
-    do
-        notice "adding $package $version deps to packlist"
-        gemfile=$(rpg-fetch "$package" "$version")
-        packagedir=$(rpg-package-register "$gemfile")
-        rpg-dependencies "$package" "$version"               |
-        sed "s/^/$package /"
-    done                                                     |
-    cat "$packlist" -                                        |
-    sort -u >> "$packlist+"
+    # Rebuild the master package list by concatenating the original user-
+    # specified packages with all dependencies of all packages solved so far.
+    # Make sure the newly built package list is sorted by the package name --
+    # the `uniq` in the `rpg-solve` pipeline above relies on this.
+    {
+        grep '^@user' "$packlist"
+        grep -v ' -$' "$solved+"             |
+        xargs -P 4 -n 2 rpg-dependencies -p
+    }  |sort -b -k 2,4                       >"$packlist+"
 
+    # Check whether the package list has changed at all. If so, new rules were
+    # added to the package list by dependencies. If not, we're done and can
+    # leave the dep solve loop.
     if cmp -s "$packlist" "$packlist+"
-    then notice "package list did not change"
-         changed=false
-    else notice "package list changed"
-         changed=true
+    then changed=false
+         notice "package list did not change"
+    else changed=true
+         notice "package list changed"
     fi
+
     mv "$packlist+" "$packlist"
+    mv "$solved+" "$solved"
 done
 
 # Check for unsolved packages in our solved list. Unsolved packages have
 # a dash "-" in their version field.
-if badpacks=$(grep '\-$' <"$sessiondir"/solved)
+if badpacks=$(grep ' -$' "$solved")
 then heed "$(echo "$badpacks" |sed -n '$=') package(s) failed to resolve:
 $badpacks"
 fi
 
 # Note the number of packages that are now queued up for installation.
-goodpacks=$(grep -v -- '\-$' <"$sessiondir"/solved)
+goodpacks=$(grep -v ' -$' "$solved")
 heed "$(echo "$goodpacks" |sed -n '$=') package(s) ready for installation:
 $goodpacks"
