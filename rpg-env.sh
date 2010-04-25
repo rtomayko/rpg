dd6a86825cf3440ef3a7d62ce5609136c5442646
Ryan Tomayko <rtomayko@gmail.com>
2010-04-24 21:20:03 -0700

    fix rpg-env not putting RPGBIN on PATH correctly

--- rpg-env.sh
+++ rpg-env.sh
@@ -13,7 +13,7 @@ If no <command> is specified, ${PROGNAME} writes the name and values
 of variables in the environment to stdout with one <name>=<value> per line.'
 
 # Put RPGBIN on PATH if it isn't there already.
-if expr "$PATH" : ".*$RPGBIN" >/dev/null &&
+if ! expr "$PATH" : ".*$RPGBIN" >/dev/null &&
    ! echo "$PATH" | tr ':' '\n' | grep -q -e "^$RPGBIN$" >/dev/null
 then PATH="$RPGBIN:$PATH"
 fi
